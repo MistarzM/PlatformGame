e7aa9803d99a3a96217dd628bba6f3968b573178
Michał Mistarz <michal.mistarz02@gmail.com>
2024-05-02 22:28:40 +0200

    Adds img upload function

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,25 +3,43 @@ package main;
 import inputs.KeyboardInputs;
 import inputs.MouseInputs;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
-import java.awt.*;
-
-import java.util.Random;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class GamePanel extends JPanel {     // JPanel -> picture
 
     private MouseInputs mouseInputs;
     private float moveRight = 0, moveDown = 0;
-    private float motionX = 0.5f, motionY = 0.3f;
-    private Color rectColor = new Color(100, 150, 20);
-    private Random random;
+    private BufferedImage img;
 
     public GamePanel(){
-        random = new Random();
         mouseInputs = new MouseInputs(this);
+
+        importImg();
+
+        setPanelSize();
         addKeyListener(new KeyboardInputs(this));
         addMouseListener(mouseInputs);
         addMouseMotionListener(mouseInputs);
+    }
+
+    private void importImg() {
+        InputStream is = getClass().getResourceAsStream("/Idle.png");
+        try {
+            img = ImageIO.read(is);
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280, 800);
+        setPreferredSize(size);
     }
 
     public void setMoveRight(int right) {
@@ -44,30 +62,6 @@ public class GamePanel extends JPanel {     // JPanel -> picture
     public void paintComponent(Graphics g){     // Graphics -> you need this to draw
         super.paintComponent(g);
 
-        moveRect();
-        g.setColor(rectColor);
-        g.fillRect((int)(moveRight), (int) (moveDown), 60, 40);
-
-    }
-
-    private void moveRect() {
-        moveRight += motionX;
-        if(moveRight >500 || moveRight < 0){
-            motionX *= -1;
-            rectColor = getRandomColor();
-        }
-        moveDown +=motionY;
-        if(moveDown > 500 || moveDown < 0){
-            motionY *= -1;
-            rectColor = getRandomColor();
-        }
-    }
-
-    private Color getRandomColor() {
-        int r = random.nextInt(0, 255);
-        int g = random.nextInt(0, 255);
-        int b = random.nextInt(0, 255);
-
-        return new Color(r, g, b);
+        g.drawImage(img.getSubimage(0, 0, 128, 64), (int)moveRight, (int)moveDown, 256, 128, null);
     }
 }
--- src/main/GameWindow.java
+++ src/main/GameWindow.java
@@ -5,11 +5,13 @@ import javax.swing.JFrame;
 public class GameWindow extends JFrame{     // JFrame -> frame
 
     public GameWindow(GamePanel gamePanel){
-        this.setSize(500, 500);
+
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        // Terminates the process when the window is closed
 
         this.add(gamePanel);
         this.setLocationRelativeTo(null);       // spawn the window in the center of our screen
+        this.setResizable(false);
+        this.pack();
         this.setVisible(true);
 
     }
