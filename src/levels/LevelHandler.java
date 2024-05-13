5581107fd7496e53fec4f7a3f6e0a250478c2b05
Hubert <kordula.hubert@gmail.com>
2024-05-13 22:08:51 +0200

    Repair background

--- src/levels/LevelHandler.java
+++ src/levels/LevelHandler.java
@@ -1,7 +1,7 @@
 package levels;
 
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.awt.Graphics;
 
 import main.Game;
 import utils.LoadAndSave;
@@ -35,9 +35,11 @@ public class LevelHandler {
     }
 
     public void draw(Graphics g, int xLevelOffset){
-        BufferedImage img = LoadAndSave.GetSpriteAtlas(LoadAndSave.LEVEL_ONE_DESIGN);
+//      BufferedImage img = LoadAndSave.GetSpriteAtlas(LoadAndSave.LEVEL_ONE_DESIGN);
+        g.setColor(new Color(25, 33, 40, 255));
+        g.fillRect(0, 0, 1920, 1080);
         levelOneBackgroundGIF.paintIcon(null, g, 0, 0);
-        g.drawImage(img, -xLevelOffset, 0, (int)(img.getWidth()* Game.SCALE), (int)(img.getHeight() * SCALE), null);
+//      g.drawImage(img, -xLevelOffset, 0, (int)(img.getWidth()* Game.SCALE), (int)(img.getHeight() * SCALE), null);
     }
 
     public void update(){
